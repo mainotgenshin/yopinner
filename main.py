4312f71198dd837698f5dbcd32232e8e61113aec
mainotgenshin <mainnotgenshin140@gmail.com>
2025-05-19 23:21:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 from telethon import TelegramClient, events
 from telethon.errors import ChatAdminRequiredError
+from flask import Flask
 
 # Replace with your credentials
 api_id = '26918101'
