3461df39e690747f6f60d5d0348c1f71b2f67c86
mainotgenshin <mainnotgenshin140@gmail.com>
2026-01-10 16:25:01 +0530

    Update fix_env.py

--- cricket_draft_bot/fix_env.py
+++ cricket_draft_bot/fix_env.py
@@ -1,6 +1,6 @@
 # fix_env.py
 content = """TELEGRAM_BOT_TOKEN=7684760520:AAGe9F2y3oUmW8XJeyB2I6FPt7xWKvWCoZY
-OWNER_IDS=7343718856,6507626305
+OWNER_IDS=7343718856
 API_ID=26918101
 API_HASH=57d6680f6549e21aca4e93c7a4221d29
 GEMINI_API_KEY=AIzaSyDHABG_wW_XImSYcoTNQ_6S--ZWIgdDWyI
@@ -13,4 +13,5 @@ MONGO_URI=mongodb+srv://cricketbotuser:NEW_PASSWORD@cluster0.j9nhzpb.mongodb.net
 with open('.env', 'w', encoding='utf-8') as f:
     f.write(content)
 print("Fixed .env encoding")
+
 
